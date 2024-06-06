ab0f0f3ad2d3810a3635be2f1eb641812afa0fbb
Andy Chang <andy.rochi@gmail.com>
2024-06-06 11:28:08 +0800

    fix: fix vite.config.ts eslint error

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,3 +1,4 @@
+// Typescript shim, see https://github.com/unplugin/unplugin-vue-markdown?tab=readme-ov-file#typescript-shim
 declare module '*.vue' {
     import type { ComponentOptions } from 'vue'
 
@@ -11,3 +12,6 @@ declare module '*.md' {
     const Component: ComponentOptions
     export default Component
 }
+
+// Declaration for markdown-it-class
+declare module 'markdown-it-class';
