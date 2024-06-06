307d4f0b34a3ad5111fa5779cfe6be39e865a408
Andy Chang <andy.rochi@gmail.com>
2024-06-06 15:53:18 +0800

    content: add home description

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import BasicLayout from '@/components/BasicLayout.vue'
+import intro from '@/content/Home/intro.md'
 </script>
 
 <template>
-  <BasicLayout>
-    <h1>Home</h1>
-  </BasicLayout>
+  <div class="w-[92vw] max-w-screen-xl p-6 py-6 mx-auto lg:px-8 lg:py-8 flex">
+    <div class="md:basis-1/2 p-6 rounded-lg bg-white">
+      <intro></intro>
+    </div>
+    <div class="md:basis-1/2"></div>
+  </div>
 </template>
