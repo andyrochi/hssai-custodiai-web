2dad0947fd4699e64d5a13eb9918cabd29738072
Andy Chang <andy.rochi@gmail.com>
2024-06-10 22:26:49 +0800

    style: change link colors

--- src/components/NavBar/NavLink.vue
+++ src/components/NavBar/NavLink.vue
@@ -1,7 +1,7 @@
 <template>
   <RouterLink
     :to="to"
-    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:text-nowrap"
+    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-600 md:p-0 md:text-nowrap"
     exactActiveClass="text-blue-700"
     @click="closeMenu"
   >
