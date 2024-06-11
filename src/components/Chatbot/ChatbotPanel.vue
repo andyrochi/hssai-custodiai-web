754e811137708e55a0ef95b753b0a29d1949b5d5
Andy Chang <andy.rochi@gmail.com>
2024-06-11 15:25:58 +0800

    feat: complete chatbot input field

--- src/components/Chatbot/ChatbotPanel.vue
+++ src/components/Chatbot/ChatbotPanel.vue
@@ -1,9 +1,89 @@
+<script setup lang="ts">
+import { ref, nextTick } from 'vue'
+
+const textarea = ref<HTMLTextAreaElement | null>(null)
+const message = ref<String | null>('')
+
+const adjustTextarea = () => {
+  if (textarea.value) {
+    textarea.value.style.height = 'auto'
+    textarea.value.style.height = `${textarea.value.scrollHeight}px`
+  }
+}
+
+const onSubmit = () => {
+  console.log(`send message: [${message.value}]`)
+  // trim message
+  const trimmedMessage = message.value?.trim()
+  if (!trimmedMessage) {
+    alert('Message cannot be empty')
+    return
+  }
+  // submit form
+  // submit complete
+  message.value = ''
+  // handle resize event
+  nextTick(adjustTextarea)
+}
+</script>
+
 <template>
-  <div>
-    <div>Le 姊家事協商好夥伴</div>
+  <div class="h-full bg-white rounded-lg shadow flex flex-col">
+    <div
+      class="px-4 py-4 border-b bg-white rounded-t-lg w-full flex align-center justify-between h-fit"
+    >
+      <div>
+        <!-- text content -->
+        <span class="font-bold">Le 姊家事協商好夥伴</span>
+        <div class="text-gray-400 text-xs">
+          請使用者以家事調解員的身分來使用，儘量公允地提供父母雙方相關資訊
+        </div>
+      </div>
+      <button type="button">
+        <!-- download button -->
+        <img src="@/assets/download-icon.svg" alt="download-icon" class="h-8" />
+      </button>
+    </div>
+    <div class="flex flex-col w-full grow bg-orange-50 rounded-b-lg">
+      <div class="grow">
+        <!-- content -->
+      </div>
+      <form class="w-full" @submit.prevent="onSubmit">
+        <label for="chat" class="sr-only">Your message</label>
+        <div
+          class="flex items-center mx-6 mb-6 px-3 py-2 border rounded-xl bg-white overflow-hidden focus-within:border-slate-300 focus-within:shadow"
+        >
+          <textarea
+            id="chat"
+            rows="1"
+            ref="textarea"
+            class="block m-0 resize-none w-full text-gray-900 bg-transparent max-h-[25dvh] max-h-52 border:none outline-none"
+            placeholder="詢問Le姊..."
+            @input="adjustTextarea"
+            @keydown.enter.exact.prevent="onSubmit"
+            v-model="message"
+          ></textarea>
+          <button
+            type="submit"
+            class="inline-flex justify-center p-2 text-orange-600 rounded-full cursor-pointer hover:bg-orange-100"
+          >
+            <svg
+              class="w-5 h-5 rotate-90 rtl:-rotate-90"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 18 20"
+            >
+              <path
+                d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z"
+              />
+            </svg>
+            <span class="sr-only">Send message</span>
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
 
 <style scoped></style>
