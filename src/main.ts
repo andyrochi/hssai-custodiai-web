bb347575bc2f3fdcf2e554df6ce996bb8a93e241
Andy Chang <andy.rochi@gmail.com>
2024-06-13 00:41:40 +0800

    dep: add PrimeVue and presets

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,19 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
 
+import Lara from '@/primevue-presets/lara'
 import './style.css'
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    unstyled: true,
+    pt: Lara
+})
 
 app.mount('#app')
