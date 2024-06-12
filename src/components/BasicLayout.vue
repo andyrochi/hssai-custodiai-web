98bd93d9a879b64aa377da9001c94fb133565ebf
Andy Chang <andy.rochi@gmail.com>
2024-06-12 10:15:12 +0800

    content: mode1 hint

--- src/components/BasicLayout.vue
+++ src/components/BasicLayout.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="w-[92vw] max-w-screen-xl shadow p-6 py-6 mx-auto lg:px-8 lg:py-8 rounded-md bg-white">
+  <div
+    class="w-[92vw] max-w-screen-xl shadow p-5 md:py-6 mx-auto lg:px-8 lg:py-8 rounded-md bg-white"
+  >
     <slot></slot>
   </div>
 </template>
--- src/views/Mode1View.vue
+++ src/views/Mode1View.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import BasicLayout from '@/components/BasicLayout.vue'
+import instructions from '@/content/Mode1/mode1_instructions.md'
 </script>
 
 <template>
   <BasicLayout>
-    <h1>模式一：選項</h1>
+    <h1 class="text-2xl font-bold">模式一：選項輸入</h1>
+    <div class="flex mt-6 pt-6 pb-3 px-4 rounded-xl bg-slate-100">
+      <instructions></instructions>
+    </div>
   </BasicLayout>
 </template>
