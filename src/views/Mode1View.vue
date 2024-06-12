b863131e0660d95df5a77c98fef492ede455b9da
Andy Chang <andy.rochi@gmail.com>
2024-06-13 02:12:40 +0800

    feat: complete model1view

--- src/views/Mode1View.vue
+++ src/views/Mode1View.vue
@@ -1,6 +1,75 @@
 <script setup lang="ts">
 import BasicLayout from '@/components/BasicLayout.vue'
 import instructions from '@/content/Mode1/mode1_instructions.md'
+import CustodyMultiSelect from '@/components/CustodyMultiSelect.vue'
+
+import { ref } from 'vue'
+
+const fatherFavFactors = ref()
+const fatherUnfavFactors = ref()
+const motherFavFactors = ref()
+const motherUnfavFactors = ref()
+
+const factorsSource = [
+  {
+    label: '親子感情',
+    value: '親子感情',
+    desc: '親子彼此互動的模式是否親密，子女是否有信賴/依附關係或害怕相處'
+  },
+  {
+    label: '意願能力',
+    value: '意願能力',
+    desc: '是否有積極(或消極)撫養的意願，相關撫養規劃與適合的親職能力'
+  },
+  {
+    label: '父母經濟',
+    value: '父母經濟',
+    desc: '收入是否穩定且足以負擔子女養育所需，是否過度負債影響生計'
+  },
+  {
+    label: '支持系統',
+    value: '支持系統',
+    desc: '其他親友是否能協助子女的生活照顧或父母的經濟支持'
+  },
+  {
+    label: '父母生活',
+    value: '父母生活',
+    desc: '居家環境、是否有足夠空間或生活作習是否合適撫養子女'
+  },
+  {
+    label: '主要照顧',
+    value: '主要照顧',
+    desc: '過往長期照顧且了解子女的生活情形，包括當前照顧的狀態是否應繼續'
+  },
+  {
+    label: '子女年齡',
+    value: '子女年齡',
+    desc: '未成年子女的年紀是否幼小需要特別照顧，還是足以清楚表達意願。'
+  },
+  {
+    label: '人格發展',
+    value: '人格發展',
+    desc: '對子女未來成長的影響(如能否穩定就學或有價值觀偏差)'
+  },
+  { label: '父母健康', value: '父母健康', desc: '心理或身體是否有不良狀況而不適任為子女照顧者' },
+  {
+    label: '父母職業',
+    value: '父母職業',
+    desc: '工作性質對子女照顧的影響(如常有夜班或出差的情形)'
+  },
+  { label: '子女意願', value: '子女意願', desc: '希望與雙親中哪一位共同生活，包括意願或態度' },
+  {
+    label: '友善父母',
+    value: '友善父母',
+    desc: '是否在子女面前誹謗對方，或阻擾對方與子女維持親子關係(含會面交往)'
+  },
+  {
+    label: '父母品行',
+    value: '父母品行',
+    desc: '是否有不良嗜好、家庭暴力、精神虐待、吸毒或入監的紀錄'
+  }
+]
+const factors = ref(factorsSource)
 </script>
 
 <template>
@@ -11,12 +80,68 @@ import instructions from '@/content/Mode1/mode1_instructions.md'
     </div>
     <div class="md:columns-2 mt-6">
       <div class="mb-6 border-b md:mb-0 md:border-r md:border-b-0">
-        <h2 class="text-xl">對父親<span class="text-green-800 font-bold">有利</span>的因素選項</h2>
-        <h2 class="text-xl">對父親<span class="text-red-800 font-bold">不利</span>的因素選項</h2>
+        <div>
+          <h2 class="text-xl">
+            對父親<span class="text-green-800 font-bold">有利</span>的因素選項
+          </h2>
+          <div class="px-2 py-2">
+            <CustodyMultiSelect
+              v-model="fatherFavFactors"
+              :options="factors"
+              optionLabel="label"
+              optionValue="value"
+              optionDesc="desc"
+              placeholder="請選擇因素"
+            >
+            </CustodyMultiSelect>
+          </div>
+        </div>
+        <div>
+          <h2 class="text-xl">對父親<span class="text-red-800 font-bold">不利</span>的因素選項</h2>
+          <div class="px-2 py-2">
+            <CustodyMultiSelect
+              v-model="fatherUnfavFactors"
+              :options="factors"
+              optionLabel="label"
+              optionValue="value"
+              optionDesc="desc"
+              placeholder="請選擇因素"
+            >
+            </CustodyMultiSelect>
+          </div>
+        </div>
       </div>
       <div>
-        <h2 class="text-xl">對母親<span class="text-green-800 font-bold">有利</span>的因素選項</h2>
-        <h2 class="text-xl">對母親<span class="text-red-800 font-bold">不利</span>的因素選項</h2>
+        <div>
+          <h2 class="text-xl">
+            對母親<span class="text-green-800 font-bold">有利</span>的因素選項
+          </h2>
+          <div class="px-2 py-2">
+            <CustodyMultiSelect
+              v-model="motherFavFactors"
+              :options="factors"
+              optionLabel="label"
+              optionValue="value"
+              optionDesc="desc"
+              placeholder="請選擇因素"
+            >
+            </CustodyMultiSelect>
+          </div>
+        </div>
+        <div>
+          <h2 class="text-xl">對母親<span class="text-red-800 font-bold">不利</span>的因素選項</h2>
+          <div class="px-2 py-2">
+            <CustodyMultiSelect
+              v-model="motherUnfavFactors"
+              :options="factors"
+              optionLabel="label"
+              optionValue="value"
+              optionDesc="desc"
+              placeholder="請選擇因素"
+            >
+            </CustodyMultiSelect>
+          </div>
+        </div>
       </div>
     </div>
     <div class="text-sm text-gray-700 mt-3">
