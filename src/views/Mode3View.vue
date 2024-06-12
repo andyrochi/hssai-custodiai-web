05210259e8a544b633e6697d8f384e7ac0e2a082
Andy Chang <andy.rochi@gmail.com>
2024-06-13 03:00:36 +0800

    feat: complete mode3 layout

--- src/views/Mode3View.vue
+++ src/views/Mode3View.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import BasicLayout from '@/components/BasicLayout.vue'
+import ModeLayout from '@/components/ModeLayout.vue'
+import instructions from '@/content/Mode3/mode3_instructions.md'
 </script>
 
 <template>
-  <BasicLayout>
-    <h1>模式三：選項加文字</h1>
-  </BasicLayout>
+  <ModeLayout title="模式三：選項加文字輸入" modeType="因素與理由">
+    <template #instructions>
+      <instructions></instructions>
+    </template>
+
+    <template #note>
+      <div class="text-sm text-gray-700 mt-3">
+        提醒：本系統目前提供兩種AI模型預測，若結果差異過大可能代表此個案不容易有效預測，請再嘗試提供更細緻的描述，重新預測。兩個模型的原理與比較請見<RouterLink
+          to="/technical-guide"
+          class="text-blue-300 hover:underline"
+          >技術說明</RouterLink
+        >。
+      </div>
+    </template>
+  </ModeLayout>
 </template>
