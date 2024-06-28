a223ab06e8a2236962425cb0612d68daa294ffad
Andy Chang <andy.rochi@gmail.com>
2024-06-28 18:33:16 +0800

    style: highlight navbar link when route matches

--- src/components/NavBar/NavLink.vue
+++ src/components/NavBar/NavLink.vue
@@ -2,7 +2,7 @@
   <RouterLink
     :to="to"
     class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-600 md:p-0 md:text-nowrap"
-    exactActiveClass="text-blue-700"
+    exactActiveClass="text-orange-700"
     @click="closeMenu"
   >
     <slot></slot>
